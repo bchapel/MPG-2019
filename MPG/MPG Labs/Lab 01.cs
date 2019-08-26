39c96f940a3a5a706170848b7a76b3c32799cd33
bchapel <svc-student@student.directory.jccc>
2019-08-26 16:09:56 -0500

    Initial Commit
    Lab 01.  Needs formatting.

---
+++ MPG/MPG Labs/Lab 01.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace MPG_Labs
+{
+    class Program
+    {
+        /* Lab 01 Test Program
+  Author: Bowen Walker Chapel
+  Version: 1.0
+  Date: August 26 2019.
+  Summary: Following Lab Procedures based on: http://jccc-mpg.wikidot.com/the-vector-class
+
+     */
+
+        static void Main(string[] args)
+        {
+            //Temporary float values used to store user input and create two vectors.
+            float tempX1;
+            float tempY1;
+            float tempZ1;
+
+            float tempX2;
+            float tempY2;
+            float tempZ2;
+
+            //Grab user input and parse as temporary float values for Vector 1.
+            Console.WriteLine("Please Input an X Coordinate for Vector 1");
+            tempX1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("Please Input an Y Coordinate for Vector 1");
+            tempY1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("Please Input an Z Coordinate for Vector 1");
+            tempZ1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            //Define new Vector using temporary float values.
+            Vector3D Vector1 = new Vector3D(tempX1, tempY1, tempZ1);
+            Console.WriteLine("");
+            Console.WriteLine("");
+
+            //Grab user input and parse as temporary float values for Vector 2.
+            Console.WriteLine("Please Input an X Coordinate for Vector 2");
+            tempX2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("Please Input an Y Coordinate for Vector 2");
+            tempY2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            Console.WriteLine("Please Input an Z Coordinate for Vector 2");
+            tempZ2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("");
+
+            Vector3D Vector2 = new Vector3D(tempX2, tempY2, tempZ2);
+
+            Console.Clear();
+            Console.WriteLine("Vector 1:");
+            Console.WriteLine("Rect:");
+            Vector1.PrintRect();
+            Vector1.PrintMag();
+            Console.WriteLine("");
+
+            Console.WriteLine("Vector 2:");
+            Console.WriteLine("Rect:");
+            Vector2.PrintRect();
+            Vector2.PrintMag();
+
+            Console.WriteLine("");
+            Console.ReadLine();
+
+            Vector3D sumVector = new Vector3D(Vector1.GetX(), Vector1.GetY(), Vector1.GetZ());
+
+            sumVector.SumRect(Vector2.GetX(), Vector2.GetY(), Vector2.GetZ());
+            Console.WriteLine("Sum of Vector 1 and 2");
+            Console.WriteLine("Rect:");
+            sumVector.PrintRect();
+            sumVector.PrintMag();
+
+            Console.ReadLine();
+            Console.WriteLine();
+
+            Vector3D subVector = new Vector3D(Vector1.GetX(), Vector1.GetY(), Vector1.GetZ());
+            subVector.SubtractRect(Vector2.GetX(), Vector2.GetY(), Vector2.GetZ());
+            Console.WriteLine("Difference of Vector 1 and 2");
+            Console.WriteLine("Rect:");
+            subVector.PrintRect();
+            subVector.PrintMag();
+
+            Console.ReadLine();
+            Console.Clear();
+
+            Vector3D scaleRect = new Vector3D(Vector1.GetX(), Vector1.GetY(), Vector1.GetZ(), Vector1.GetW());
+
+            scaleRect.ScaleRect(scaleRect.GetW());
+            Console.WriteLine("Result of Scaling Vector 1 by it's provided Scale value");
+            Console.WriteLine("Scale Value is: " + Vector1.GetW());
+            Console.WriteLine("Rect:");
+            scaleRect.PrintRect();
+            scaleRect.PrintMag();
+
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.WriteLine("Vector 1 expressed as the product of it's magnitude and unit direction");
+
+            if (Vector1.GetMag() != 0)
+                Vector1.SetRectGivenRect(Vector1.GetX() / Vector1.GetMag(), Vector1.GetY() / Vector1.GetMag(), Vector1.GetZ() / Vector1.GetMag());
+            else
+            {
+                Console.WriteLine("ERROR: Can not complete operation.  Magnitude is zero");
+            }
+
+            
+            Vector1.PrintRect();
+
+            Console.WriteLine();
+            Console.WriteLine("End of Program. Press Enter to close");
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ MPG/MPG Labs/Vector3D.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MPG_Labs
+{
+    class Vector3D
+    {
+        /* Vector 3D class
+          Author: Bowen Walker Chapel
+          Version: 1.0
+          Date: August 26 2019.
+          Summary: 
+         
+             */
+
+        //Stored X, Y, Z, coordinates of this Vector, and W scalar.
+        private float X;
+        private float Y;
+        private float Z;
+        private float W = 1;
+
+
+        //Initializing the X, Y, and Z values for this vector. 
+        public Vector3D(float initX, float initY, float initZ)
+        {
+            X = initX;
+            Y = initY;
+            Z = initZ;
+        }
+        //Overloaded constructor allowing for additional defining of a W value so user doesn't always need a W value.
+        public Vector3D(float initX, float initY, float initZ, float initW)
+        {
+            X = initX;
+            Y = initY;
+            Z = initZ;
+            W = initW;
+        }
+
+        //Set the X, Y, and Z values of the Vector via external input.
+        public void SetRectGivenRect(float inputX, float inputY, float inputZ)
+        {
+            X = inputX;
+            Y = inputY;
+            Z = inputZ;
+        }
+
+        //Overloaded SetRectGivenRect function, allowing for inputing a W value.
+        public void SetRectGivenRect(float inputX, float inputY, float inputZ, float inputW)
+        {
+            X = inputX;
+            Y = inputY;
+            Z = inputZ;
+            W = inputW;
+        }
+        //Print the X, Y, and Z coordinate of this vector.
+        public void PrintRect()
+        {
+            Console.WriteLine("(" + X + ", " + Y + ", " + Z + ")");
+        }
+
+        //Print the magnitude of this vector. Square Root of ( x*x + y*y + z*z)
+        public void PrintMag()
+        {
+            float magnitude = GetMag();
+            Console.WriteLine("Magnitude: " + magnitude);
+        }
+        //Return the private X value.
+        public float GetX()
+        {
+            return X;
+        }
+        //Return the private Y value.
+        public float GetY()
+        {
+            return Y;
+        }
+        //Return the private Z value.
+        public float GetZ()
+        {
+            return Z;
+        }
+
+        //Returns the private W value.
+        public float GetW()
+        {
+            return W;
+        }
+
+        //Calculate and return the magnitude of the vector.
+        public float GetMag()
+        {
+            float tempMagSquare = (X * X) + (Y * Y) + (Z * Z);
+            if (tempMagSquare != 0)
+                return (float)Math.Sqrt(tempMagSquare);
+            else
+            {
+                Console.WriteLine("ERROR: Output is Zero.  Is this a Zero vector?");
+                return 0;
+            }
+        }
+
+        //Calculate and return the SQUARED magnitude of the vector.
+        public float GetMagSq()
+        {
+            float tempMagSquare = (X * X) + (Y * Y) + (Z * Z);
+
+            if (tempMagSquare != 0)
+                return tempMagSquare;
+            else
+            {
+                Console.WriteLine("ERROR: Output is Zero.  Is this a Zero vector?");
+                return 0;
+            }
+        }
+
+        //Adds the input xyz values to the X Y Z of this vector.
+        public void SumRect(float x, float y, float z)
+        {
+            X += x;
+            Y += y;
+            Z += z;
+        }
+
+        //Subtracts the input values from the Vector.
+        public void SubtractRect(float x, float y, float z)
+        {
+            X -= x;
+            Y -= y;
+            Z -= z;
+        }
+
+        //Scales/Multiplies the rect by the input values.
+        public void ScaleRect(float input)
+        {
+            X *= input;
+            Y *= input;
+            Z *= input;
+        }
+
+    }
+}
