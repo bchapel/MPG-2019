ac2911e5667e9278999b25c5caf362fdd9427121
bchapel <svc-student@student.directory.jccc>
2019-11-18 16:46:14 -0600

    Lab Selector Improvements.

--- MPG/MPG Labs/LabSelector.cs
+++ MPG/MPG Labs/LabSelector.cs
@@ -29,43 +29,56 @@ namespace MPG_Labs
             switch (choice)
             {
                 case 1:
+                    Console.Clear();
                     Lab_01 lab1 = new Lab_01();
                     break;
                 case 2:
+                    Console.Clear();
                     Lab_02 lab2 = new Lab_02();
                     break;
                 case 3:
+                    Console.Clear();
                     Lab03 lab3 = new Lab03();
                     break;
                 case 4:
+                    Console.Clear();
                     Lab04 lab4 = new Lab04();
                     break;
                 case 5:
+                    Console.Clear();
                     Lab05 lab5 = new Lab05();
                     break;
                 case 6:
+                    Console.Clear();
                     Lab06 lab6 = new Lab06();
                     break;
                 case 7:
+                    Console.Clear();
                     Lab07 lab7 = new Lab07();
                     break;
                 case 8:
+                    Console.Clear();
                     Lab08 lab8 = new Lab08();
                     break;
                 case 9:
+                    Console.Clear();
                     //Lab09 lab9 = new Lab09();
                     break;
                 case 10:
+                    Console.Clear();
                     Lab10 lab10 = new Lab10();
                     break;
                 case 11:
+                    Console.Clear();
                     Lab11 lab11 = new Lab11();
                     break;
                 case 12:
+                    Console.Clear();
                     //Lab12 lab12 = new Lab12();
                     break;
 
                 case 13:
+                    Console.Clear();
                     //Lab13 lab13 = new Lab13();
                     break;
             }
